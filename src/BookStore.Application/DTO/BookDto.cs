4318ddc9140e22c3ffd4f20cbed126040a134771
PhucGrut <ngotronghl52@gmail.com>
2025-10-01 21:11:37 +0700

    Add project files.

---
+++ src/BookStore.Application/DTO/BookDto.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace BookStore.DTO
+{
+    public class BookDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+    }
+
+    public class CreateUpdateBookDto
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public BookType Type { get; set; } = BookType.Undefined;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public float Price { get; set; }
+    }
+}
---
+++ src/BookStore.Application/Mapper/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BookStore.DTO;
+using BookStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Mapper
+{
+    public class BookStoreApplicationAutoMapperProfile : Profile
+    {
+        public BookStoreApplicationAutoMapperProfile()
+        {
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateUpdateBookDto, Book>();
+        }
+    }
+}
